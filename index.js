0bd9fc3c1fe01539c9f1f11c61bb80841002b2a3
Naileth Rivero <nailethhdarina@gmail.com>
2022-03-24 09:20:50 -0300

    agregando interfaz de captura

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+const readline = require('readline');
+
+let interfazCaptura = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+
+});
+
+interfazCaptura.question('¿Cuál es su ruta?: ', function(answer){
+  console.log(`Su ruta es: ${answer}`);
+
+  interfazCaptura.close();
+
+})
