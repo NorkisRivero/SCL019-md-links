10c14b802abf2f27924e346a7a1c259e846c0197
Norkis Rivero <norkysdayana1@gmail.com>
2022-03-28 14:47:36 -0300

    funciones para ver archivo o directorio, y si tiene extensión .md

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
 const mdlinks = require ('./index.js');
 const path = require('path');
+let { lstatSync, readdirSync } = require('fs');
+
 
 // const path = process.argv[2];
 
@@ -12,6 +14,17 @@ const path = require('path');
  const convertPath = (answer) => (path.isAbsolute(answer) ? answer : path.resolve(answer)); 
 
 
+ //Ver si la ruta es un archivo
+ const isFile = (answer) => lstatSync(answer).isFile(); 
+
+// verifico la extensión del archivo
+const verifyExtensionMD = (resp) => path.extname(resp) === '.md';
+
+// // Leyendo directories
+// const readDirectory = (route) => readdirSync(route); 
 
 
  exports.convertPath = convertPath;
+ exports.isFile = isFile;
+ exports.verifyExtensionMD = verifyExtensionMD;
+ exports.readDirectory = readDirectory;
